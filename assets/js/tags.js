99dad7f8d20950523180ffa10d5635a8f0097b76
Sharundaar <rabiller.donatien@hotmail.fr>
2023-02-12 11:34:14 -0500

    Fix spamming scroll error in tags.js

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -143,10 +143,13 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const tags = urlParams.getAll("tag");
 filterTags(tags);
-window.onscroll = function(){
-      if (window.scrollY > (window.innerHeight * 0.5)) {
-        document.querySelector('.down-arrow').classList.add('fade');
-      } else{
-        document.querySelector('.down-arrow').classList.remove('fade');
-      }
-    };
+window.onscroll = function () {
+    const down_arrow = document.querySelector('.down-arrow');
+    if (down_arrow) {
+        if (window.scrollY > (window.innerHeight * 0.5)) {
+            down_arrow.classList.add('fade');
+        } else {
+            down_arrow.classList.remove('fade');
+        }
+    }
+};
