7b59a2b1701d4d258dfc821f5bcc818325f7bd5c
benui <ben@benui.ca>
2025-02-16 16:09:16 -0800

    Fixed countdown, theming

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,34 +1,33 @@
 let countdown = document.getElementById('countdown');
 
-if(countdown !== null)
+if (countdown !== null)
 {
-    const nf = new Intl.NumberFormat("en-US", {
-    });
-    const NOTGDC_START_DATE = new Date(Date.UTC(2024, 02, 18, 16, 0, 0));
-    const NOTGDC_END_DATE = new Date(Date.UTC(2024, 02, 22, 16, 0, 0));
+    const number_format = new Intl.NumberFormat("en-US", {});
+    const NOTGDC_START_DATE = new Date(Date.UTC(2025, 2, 10, 16, 0, 0));
+    const NOTGDC_END_DATE = new Date(Date.UTC(2025, 2, 14, 16, 0, 0));
 
     function format_date(weeks, days, hours, minutes, seconds)
     {
-        let countdown_string = "Starts in ";
+        let countdown_string = "Starts in";
         if (weeks > 0)
         {
-            countdown_string += weeks + (weeks > 1 ? " Weeks " : " Week ");
+            countdown_string += " " + weeks + (weeks > 1 ? " Weeks" : " Week");
         }
         if (days > 0)
         {
-            countdown_string += (weeks > 0 ? "and " : "") + days + ( days > 1 ? " Days " : " Day ");
+            countdown_string += (weeks > 0 ? " and " : " ") + days + ( days > 1 ? " Days" : " Day");
         }
         if (days < 3 && hours > 0 && weeks == 0)
         {
-            countdown_string += nf.format(hours) + (hours > 1 ? " Hours " : " Hour ");
+            countdown_string += " " + number_format.format(hours) + (hours > 1 ? " Hours" : " Hour");
         }
 
         if (days == 0 && weeks == 0)
         {
-            countdown_string += nf.format(minutes) + (minutes > 1 ? " Minutes " : " Minute ");
+            countdown_string += " " + number_format.format(minutes) + (minutes > 1 ? " Minutes" : " Minute");
         }
 
-        return countdown_string;
+        return countdown_string + "!";
     }
 
     function update_countdown() {
@@ -36,37 +35,34 @@ if(countdown !== null)
         var diff = NOTGDC_START_DATE - now_date;
         if (diff > 0)
         {
-            const _MS_PER_SECONDS = 1000;
-            const _MS_PER_MINUTES = _MS_PER_SECONDS * 60;
-            const _MS_PER_HOUR = _MS_PER_MINUTES * 60;
-            const _MS_PER_DAY = _MS_PER_HOUR * 24;
-            const _MS_PER_WEEK = _MS_PER_DAY * 7;
+            const MS_PER_SECONDS = 1000;
+            const MS_PER_MINUTES = MS_PER_SECONDS * 60;
+            const MS_PER_HOUR = MS_PER_MINUTES * 60;
+            const MS_PER_DAY = MS_PER_HOUR * 24;
+            const MS_PER_WEEK = MS_PER_DAY * 7;
         
-            const weeks   = Math.floor(diff / _MS_PER_WEEK);    diff -= weeks * _MS_PER_WEEK;
-            const days    = Math.floor(diff / _MS_PER_DAY);     diff -= days * _MS_PER_DAY;
-            const hours   = Math.floor(diff / _MS_PER_HOUR);    diff -= hours * _MS_PER_HOUR;
-            const minutes = Math.floor(diff / _MS_PER_MINUTES); diff -= minutes * _MS_PER_MINUTES;
-            const seconds = Math.floor(diff / _MS_PER_SECONDS);
-
+            const weeks   = Math.floor(diff / MS_PER_WEEK);    diff -= weeks   * MS_PER_WEEK;
+            const days    = Math.floor(diff / MS_PER_DAY);     diff -= days    * MS_PER_DAY;
+            const hours   = Math.floor(diff / MS_PER_HOUR);    diff -= hours   * MS_PER_HOUR;
+            const minutes = Math.floor(diff / MS_PER_MINUTES); diff -= minutes * MS_PER_MINUTES;
+            const seconds = Math.floor(diff / MS_PER_SECONDS);
             
             countdown.innerText = format_date(weeks, days, hours, minutes, seconds);
         }
         else
         {
             var end_diff = NOTGDC_END_DATE - now_date;
-            if(end_diff > 0)
+            if (end_diff > 0)
             {
-                countdown.innerText = "Event in progress !"
+                countdown.innerText = "Happening now!"
             }
             else
             {
-                // countdown.innerText = "Event's over ! Thanks for joining !"
+                countdown.innerText = "Event's over! Thanks for joining!"
                 countdown.remove();
             }
         }
     }
 
-    // Don't update dynamically come on
-    //setInterval(update_countdown, 1000);
     update_countdown();
 }
